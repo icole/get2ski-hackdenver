30cecc1e7b337513c8bd4d5d1a9ab1407e504a0d
Ian Cole <icole@teliax.com>
2012-08-18 15:10:51 -0600

    Update sinatra routs

--- server.rb
+++ server.rb
@@ -1,11 +1,30 @@
 require 'sinatra'
 
 set :public_folder, Proc.new { File.join(root, "static") }
-set :destination, "blhblahblah"
+set :destination, "default"
 
 get '/' do
-  erb :locations
-  set :destination, params
+  erb :index
+end
+
+post '/vail' do
+  set :destination, ""
+  erb :mapquest
+end
+
+post '/breck' do
+  set :destination, ""
+  erb :mapquest
+end
+
+post '/keystone' do
+  set :destination, ""
+  erb :mapquest
+end
+
+post '/winterpark' do
+  set :destination, ""
+  erb :mapquest
 end
 
 get '/stations' do
