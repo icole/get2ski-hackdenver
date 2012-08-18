5ec4ef7a882dd146d79636349a497895e0c1e345
Andy Ennamorato <virtualandy@gmail.com>
2012-08-17 23:18:43 -0600

    load the Berthoud cam

--- mq_controls.js
+++ mq_controls.js
@@ -8,7 +8,7 @@
       var options={
         elt:document.getElementById('map'),       /*ID of element on the page where you want the map added*/ 
         zoom:13,                                  /*initial zoom level of the map*/ 
-        latLng:{lat:40.735383, lng:-73.984655},   /*center of map in latitude/longitude */ 
+        latLng:{lat:40.320969, lng:-104.980774},   /*center of map in latitude/longitude */ 
         mtype:'osm',                              /*map type (osm)*/ 
         bestFitMargin:0,                          /*margin offset from the map viewport when applying a bestfit on shapes*/ 
         zoomOnDoubleClick:true                    /*zoom in when double-clicking on map*/ 
@@ -19,9 +19,29 @@
       console.log('map loaded');
     }
     
+    function loadWebcams() {
+    
+    /*Using the MQA.Poi constructor*/ 
+  	var berthoud_cam=new MQA.Poi({lat:40.320969, lng:-104.980774});
+  	var camIcon=new MQA.Icon("http://cotrip.org/theme/cotrip.org/images/cameraTour/icon_camera_still_22x20.gif",22,20);
+
+  	/*Sets the rollover content of the POI.*/ 
+  	berthoud_cam.setRolloverContent('View the BERTHOUD station web camera');
+
+  	/*Sets the InfoWindow contents for the POI. By default, when the POI receives a mouseclick 
+  	event, the InfoWindow will be displayed with the HTML passed in to MQA.POI.setInfoContentHTML method.*/ 
+  	berthoud_cam.setInfoContentHTML('Camera type: Still. Images Courtesy of ITS <img style="width:300px; height:200px;" src="http://cotrip.org/images/ws/camera?imageURL=77"/>');
+  	berthoud_cam.setIcon(camIcon);
+  	berthoud_cam.maxInfoWindowWidth = 420;
+
+  	/*This will add the POI to the map in the map's default shape collection.*/ 
+  	map.addShape(berthoud_cam);
+    }
+    
     $(document).bind('pageinit', function(event) {
     	console.log('page loaded');
     	loadMap();
+    	loadWebcams();
     });
     
     console.log('page downloaded');
