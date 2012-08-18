84a87e73aa6fa2e4d7a5970425bc8be0f8dfc970
Andy Ennamorato <virtualandy@gmail.com>
2012-08-18 17:39:27 -0600

    winterpark

--- static/js/mq_controls.js
+++ static/js/mq_controls.js
@@ -157,7 +157,7 @@
         			 resort.lat = 39.605;
         			 resort.lng = -105.954167;
         			break;
-        			case /Winter/.test(resortName):
+        			case /winterpark/.test(resortName):
         			 resort.name = resortName;
         			 resort.lat = 39.886944;
         			 resort.lng = -105.7625;
